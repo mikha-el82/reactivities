227f1e927959ac7360973c9e25a118105f11f8a2
Michal <michal@jopixel.cz>
2026-02-22 22:31:47 +0100

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -4,16 +4,20 @@ using Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
     {
         opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
     });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(options =>
+    options.AllowAnyHeader().AllowAnyMethod().
+        WithOrigins("http://localhost:5173", "https://localhost:5173"));
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
